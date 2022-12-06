ac15c1bcebe19341a432e8f8fb16cc47b2fd99d3
Reece English <englishra.dev@icloud.com>
2022-12-06 19:07:57 +0000

    Created Integrations.js

--- src/pages/Integrations.js
+++ src/pages/Integrations.js
@@ -1,13 +1,13 @@
-// Dashboard.js
+// Integrations.js
 // iDSimplify Frontend
 // Created by Reece English on 06.12.2022
 
-const Dashboard = () => {
+const Integrations = () => {
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Integrations</h1>
         </div>
     );
 };
-        
-export default Dashboard;
+
+export default Integrations;
