ad04021c1506c0495fa4dd215eacfb32110306db
english-ra <englishra.dev@icloud.com>
2023-02-19 00:49:29 +0000

    Create CreateRootOrganisation.js

---
+++ src/pages/Join/CreateRootOrganisation.js
@@ -0,0 +1,13 @@
+// CreateRootOrganisation.js
+// iDSimplify Frontend
+// Created by Reece English on 06.12.2022
+
+const CreateRootOrganisation = () => {
+    return (
+        <div>
+            <h1>CreateRootOrganisation</h1>
+        </div>
+    );
+};
+        
+export default CreateRootOrganisation;
