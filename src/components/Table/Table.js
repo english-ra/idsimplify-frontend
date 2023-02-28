e8ff670c8157c0f6759011d6e3681741ebe1aae9
english-ra <englishra.dev@icloud.com>
2023-02-28 03:07:36 +0000

    - Created a circular button - Working on the ability for custom columns

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -5,13 +5,18 @@
 import TableHeading from "./TableHeading";
 
 import classes from './Table.module.css';
+import CircularButton from "../Buttons/CircularButton";
 
 const Table = (props) => {
     return (
-        <table className={classes.table}>
-            <TableHeading headings={props.headings} />
-            {props.children}
-        </table>
+        <div className={classes.div}>
+            <table className={classes.table}>
+                <TableHeading headings={props.headings} />
+                {props.children}
+            </table>
+
+            <CircularButton text='...' />
+        </div>
     );
 };
 
