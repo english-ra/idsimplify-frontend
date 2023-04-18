78e564aae4d5824b6b4b41fdef741d464056577e
english-ra <englishra.dev@icloud.com>
2023-04-18 12:12:24 +0100

    .

--- src/components/AcceptDenyBox/AcceptDenyBox.js
+++ src/components/AcceptDenyBox/AcceptDenyBox.js
@@ -9,15 +9,24 @@ import LayoutInner from '../../components/layout/LayoutInner';
 import classes from './AcceptDenyBox.module.css';
 
 const AcceptDenyBox = (props) => {
+
+    const data = props.data;
+    const headingText = props.headingText;
+    const subheadingText = props.subheadingText;
+
+    const acceptInvitationHandler = () => { props.onAccept(data); };
+    const denyInvitationHandler = () => { props.onDeny(data); };
+
     return (
         <div className={`${classes.root} ${props.className}`}>
             <div className={classes.textDiv}>
-                <h4>Tenancy Name</h4>
+                <h4>{headingText}</h4>
+                <p>{subheadingText}</p>
             </div>
 
             <div className={classes.buttonDiv}>
-                <button>Accept</button>
-                <button>Deny</button>
+                <button className={classes.accept} onClick={acceptInvitationHandler}>Accept</button>
+                <button className={classes.deny} onClick={denyInvitationHandler}>Deny</button>
             </div>
         </div>
     );
